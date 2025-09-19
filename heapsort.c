79dc8417fbeb18aef21a4faf9925e5879439ecf5
Adrian-LP <lopezcruzjacieladrian@gmail.com>
2025-09-19 08:56:13 -0600

    Update heapsort.c
    Lopez Cruz Jaciel Adrian 
    Miguel Mata Jared

--- heapsort.c
+++ heapsort.c
@@ -17,21 +17,23 @@ void max_heapify(int a[], int n, int i);
 void build_max_heap(int a[], int n);
 void heap_sort(int a[], int n);
 
-/* MAIN de prueba mínima */
+
 int main(void) {
-    int n; if (scanf("%d", &n) != 1 || n <= 0) return 0;
+    int n;printf("Ingresa el tamaño de tu arreglo:\n"); if (scanf("%d", &n) != 1 || n <= 0) return 0;
     int *a = (int*)malloc(sizeof(int) * n);
     if (!a) return 0;
-    for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
+    for (int i = 0; i < n; ++i) {
+        printf("Ingresa tu elemento #%d del arreglo:\n", i + 1);
+        scanf("%d", &a[i]);
+    }
 
     heap_sort(a, n);
-
+    printf("Tu arreglo ordenado es:\n");
     for (int i = 0; i < n; ++i) {
         if (i) putchar(' ');
         printf("%d", a[i]);
     }
-    putchar('
-');
+    putchar(' ');
 
     free(a);
     return 0;
@@ -39,13 +41,43 @@ int main(void) {
 
 /* IMPLEMENTAR AQUÍ */
 void max_heapify(int a[], int n, int i) {
-    /* TODO */
+    int left = 2*i + 1;
+    int right = 2*i + 2; 
+    int largest = i;
+
+    if (left < n && a[left] > a[largest]) 
+        largest = left;
+    if (right < n && a[right] > a[largest]) 
+        largest = right;
+
+    if (largest != i) {
+        int tmp = a[i];
+        a[i] = a[largest];
+        a[largest] = tmp;
+
+       
+        max_heapify(a, n, largest);
+    }
 }
+
+
 
 void build_max_heap(int a[], int n) {
-    /* TODO */
+    for (int i = n/2 - 1; i >= 0; --i) {
+        max_heapify(a, n, i);
+    }
 }
 
+
 void heap_sort(int a[], int n) {
-    /* TODO */
+     build_max_heap(a, n);
+    for (int end = n - 1; end > 0; --end) {
+        
+        int tmp = a[0];
+        a[0] = a[end];
+        a[end] = tmp;
+
+        
+        max_heapify(a, end, 0);
+    }
 }
